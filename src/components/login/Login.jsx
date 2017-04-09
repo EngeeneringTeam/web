f2a6954ed7e22c81f16e6b9302490ae8383ae768
Mateusz <kontakt@mateuszoracz.pl>
2017-04-09 11:10:52 +0200

    TAB-6 Added Example login component with stylesheet

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
+
+import ExampleFlatButton from '../utils/exampleFlatButton.jsx';
 
 class Login extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="login-component">
         Login component
+        <ExampleFlatButton
+          onClick={this.onPressButton.bind(this)}
+        >
+          {'Go to Register Component'}
+        </ExampleFlatButton>
       </div>
     );
+  }
+
+  onPressButton() {
+    browserHistory.push('/register');
   }
 
 }
