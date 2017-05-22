5c927dbb739709aac212894d62ad1e4472ae9012
Paweł Graczyk <pawel.graczyk@rsqtechnologies.com>
2017-05-22 17:33:27 +0200

    TAB-53 Added simple comments panel

--- src/app/components/restaurant/Restaurant.jsx
+++ src/app/components/restaurant/Restaurant.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import { object, func } from 'prop-types';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-
+import FlatButton from '../utils/FlatButton.jsx';
 // import Reservation from './Reservation.jsx';
 
 class Restaurant extends React.Component {
@@ -42,10 +42,12 @@ class Restaurant extends React.Component {
         </div> */}
 
         <div className="grid-flex">
-          <div className="forty">
-            <p>.forty</p>
+          <div className="forty item">
+            <div className="test">
+              
+            </div>
           </div>
-          <div className="sixty">
+          <div className="sixty item">
             <p>.sixty</p>
           </div>
         </div>
@@ -53,13 +55,36 @@ class Restaurant extends React.Component {
         <div className="grid-flex">
           <div className="full">
 
-            <Tabs defaultIndex={0}>
+            <Tabs defaultIndex={1}>
               <TabList>
-                <Tab>Title 1</Tab>
-                <Tab>Title 2</Tab>
+                <Tab>Informacje</Tab>
+                <Tab>Komentarze</Tab>
               </TabList>
-              <TabPanel>as</TabPanel>
-            <TabPanel>dd</TabPanel>
+              <TabPanel>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </TabPanel>
+              <TabPanel>
+                <div className="comments">
+                    <div className="comment-wrap">
+                      <div className="item">
+                        <textarea placeholder="Wpisz komentarz..."/>
+                      </div>
+                    </div>
+                		<div className="comment-wrap">
+                				<div className="photo">
+                          <img className="avatar" src="/assets/photo.jpg"/>
+                				</div>
+                				<div className="comment-block item">
+                            <div className="top-comment">
+                              <p>Nazwa użytkownika</p>
+                              <p>27 maj 2017</p>
+                            </div>
+                						<p className="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto temporibus iste nostrum dolorem natus recusandae incidunt voluptatum. Eligendi voluptatum ducimus architecto tempore, quaerat explicabo veniam fuga corporis totam reprehenderit quasi
+                								sapiente modi tempora at perspiciatis mollitia, dolores voluptate. Cumque, corrupti?</p>
+                				</div>
+                		</div>
+                  </div>
+              </TabPanel>
             </Tabs>
 
           </div>
