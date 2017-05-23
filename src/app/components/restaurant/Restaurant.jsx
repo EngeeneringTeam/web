c8fe6a119244e801f9dd63eabf7b6a4ae08cbbfd
Paweł Graczyk <pawel.graczyk@rsqtechnologies.com>
2017-05-23 10:58:55 +0200

    TAB-53 Update restaurant profile view

--- src/app/components/restaurant/Restaurant.jsx
+++ src/app/components/restaurant/Restaurant.jsx
@@ -1,68 +1,73 @@
 import React from 'react';
-// import { object, func } from 'prop-types';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import FlatButton from '../utils/FlatButton.jsx';
-// import Reservation from './Reservation.jsx';
+import StarSVG from '../svg/StarSVG.jsx';
 
 class Restaurant extends React.Component {
   render() {
     return (
       <div className="grid-flex-container">
-        {/* <div className="grid-flex image">
+        <div className="grid-flex image">
           <div className="full">
-            <img src="/assets/photo-2.jpg"/>
+            <img src="/assets/photo-bg.jpg"/>
           </div>
-        </div> */}
-        {/* <div className="grid-flex">
-          <div className="ten">
-            <p>.ten</p>
-          </div>
-          <div className="ninety">
-            <p>.ninety</p>
-          </div>
-        </div> */}
-
-        {/* <div className="grid-flex">
-          <div className="twenty">
-            <p>.twenty</p>
-          </div>
-          <div className="eighty">
-            <p>.eighty</p>
-          </div>
-        </div> */}
-
-        {/* <div className="grid-flex">
-          <div className="thirty">
-            <p>.thirty</p>
-          </div>
-          <div className="seventy">
-            <p>.seventy</p>
-          </div>
-        </div> */}
-
+        </div>
         <div className="grid-flex">
-          <div className="forty item">
-            <div className="test">
-              
+          <div className="forty info">
+            <h1>{this.props.restaurant.name}</h1>
+            <h3>{this.props.restaurant.street}<br/>{this.props.restaurant.city}</h3>
+            <FlatButton full centered>{'Rezerwacja'}</FlatButton>
+          </div>
+          <div className="sixty rating">
+            <div className="average">
+                <h3>Średnia ocena użytkowników</h3>
+              <h1>{this.props.restaurant.rate}<span>/5</span></h1>
+                <div className="rate">
+                  <StarSVG/><StarSVG/><StarSVG/><StarSVG/><StarSVG/>
+                  <p>123 oddanych głosów</p>
+                </div>
             </div>
-          </div>
-          <div className="sixty item">
-            <p>.sixty</p>
-          </div>
+              <div className="graph-panel">
+                <div className="bar-graph-container">
+                  <div className="bar-container">
+                     <div className="five">
+                       <p>5</p>
+                      </div>
+                  </div>
+                  <div className="bar-container">
+                     <div className="four">
+                       <p>4</p>
+                      </div>
+                  </div>
+                  <div className="bar-container">
+                     <div className="three">
+                       <p>3</p>
+                      </div>
+                  </div>
+                  <div className="bar-container">
+                     <div className="two">
+                       <p>2</p>
+                      </div>
+                  </div>
+                  <div className="bar-container">
+                     <div className="one">
+                       <p>1</p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+           </div>
         </div>
 
         <div className="grid-flex">
           <div className="full">
 
-            <Tabs defaultIndex={1}>
+            <Tabs defaultIndex={0}>
               <TabList>
-                <Tab>Informacje</Tab>
                 <Tab>Komentarze</Tab>
+                <Tab>Informacje</Tab>
               </TabList>
-              <TabPanel>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </TabPanel>
               <TabPanel>
                 <div className="comments">
                     <div className="comment-wrap">
@@ -85,45 +90,12 @@ class Restaurant extends React.Component {
                 		</div>
                   </div>
               </TabPanel>
+              <TabPanel>
+                <p>{this.props.restaurant.description}</p>
+              </TabPanel>
             </Tabs>
-
           </div>
         </div>
-        {/* <div className="grid-flex">
-          <div className="fifty">
-            <p>.fifty</p>
-          </div>
-          <div className="fifty">
-            <p>.fifty</p>
-          </div>
-        </div> */}
-
-        {/* <div className="grid-flex">
-          <div className="quarter">
-            <p>.quarter</p>
-          </div>
-          <div className="threequarter">
-            <p>.threequarter(s)</p>
-          </div>
-        </div> */}
-
-        {/* <div className="grid-flex">
-          <div className="third">
-            <p>.third</p>
-          </div>
-          <div className="twothird">
-            <p>.twothird(s)</p>
-          </div>
-        </div> */}
-
-        {/* <div className="grid-flex">
-          <div className="half">
-            <p>.half</p>
-          </div>
-          <div className="half">
-            <p>.half</p>
-          </div>
-        </div> */}
       </div>
     );
   }
